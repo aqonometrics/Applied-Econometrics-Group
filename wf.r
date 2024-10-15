c37585e2bb946fb5458c7ed5fc0bd2789f12d48f
aqonometrics <hello.afiqphy@gmail.com>
2024-10-15 14:04:29 +0800

    Update wf.r

--- wf.r
+++ wf.r
@@ -1,2 +1,7 @@
 # If have not install readxl
 install.packages("readxl")
+
+# Include the libraries needed for the project
+library(readxl) # to include data from excel
+library(stargazer) # to present finding
+
